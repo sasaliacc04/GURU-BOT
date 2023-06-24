7a6df2e1a3d83979415f27e2c8bc0aad9ff33239
sasaliacc04 <137411123+sasaliacc04@users.noreply.github.com>
2023-06-24 22:52:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['+94718002388', 'GURU', true],
+  ['+94718002388', 'ᴋɪɴɢ_ꜱᴀꜱᴍɪᴛʜᴀ', true], 
   [''] 
 ] //Number of owners
 
